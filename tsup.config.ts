2ff53f1c771ed6cf529665ce5288e6c9b978d300
Dagim G. Astatkie <workflow.jd@gmail.com>
2025-11-18 10:22:46 +0300

    feat: switch to tsup for publishing compiled JS instead of TS source

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+	entry: ["src/index.ts"],
+	format: ["esm"],
+	dts: true,
+	clean: true,
+	outDir: "dist",
+	target: "es2020",
+	keepNames: true,
+});
